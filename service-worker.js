862c1a869703fca88509289165b8c05d5496ebe9
Sergey-ultra <maasa@list.ru>
2020-08-22 16:15:19 +0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Realstate/precache-manifest.70abf9a9e472565ad63ea269809be631.js"
+  "/Realstate/precache-manifest.523c864e9b096047c262ee14b8b14942.js"
 );
 
 self.addEventListener('message', (event) => {
