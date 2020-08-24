91f4a7e22ea385498ddde0e83469069b32d530bf
Sergey-ultra <maasa@list.ru>
2020-08-24 18:43:45 +0400

    Updates

---
+++ precache-manifest.38e17026e6db7a52b0a92d31835920da.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f5c77185401f1977dd8dab55b0546b3",
+    "url": "/Realstate/index.html"
+  },
+  {
+    "revision": "6356d9d4e12560d2f6e4",
+    "url": "/Realstate/static/css/main.76a25ad5.chunk.css"
+  },
+  {
+    "revision": "29c8cb683b00795111ae",
+    "url": "/Realstate/static/js/2.32aab3cc.chunk.js"
+  },
+  {
+    "revision": "ae247c52cb5532e362a980244e834a93",
+    "url": "/Realstate/static/js/2.32aab3cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6356d9d4e12560d2f6e4",
+    "url": "/Realstate/static/js/main.30a6286a.chunk.js"
+  },
+  {
+    "revision": "1bab7010b234482d5588",
+    "url": "/Realstate/static/js/runtime-main.94ebe41b.js"
+  },
+  {
+    "revision": "306b00fdedfaa4f721a6478646efefd4",
+    "url": "/Realstate/static/media/1421138632_frozen-480x800.306b00fd.jpg"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/Realstate/static/media/1_9EBHIOzhE1XfMYoKz1JcsQ.26ef8bb4.gif"
+  },
+  {
+    "revision": "f2a12fb94176a355522aa0d5c603535e",
+    "url": "/Realstate/static/media/ic_head_logo.f2a12fb9.svg"
+  },
+  {
+    "revision": "34a4f31c87f0e3f9e7c682ebf7eb3664",
+    "url": "/Realstate/static/media/left-arrow.34a4f31c.svg"
+  },
+  {
+    "revision": "5e5b72cc72ffd2c9ea04f8a4742dd6b1",
+    "url": "/Realstate/static/media/right-arrow.5e5b72cc.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Realstate/precache-manifest.5aeff6da434bb5f71feb019885d91495.js"
+  "/Realstate/precache-manifest.38e17026e6db7a52b0a92d31835920da.js"
 );
 
 self.addEventListener('message', (event) => {
